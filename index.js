b573b617590977b02322219465e7ea0ca593e865
unknown <hamayoon.47611@iqra.edu.pk>
2023-02-04 16:16:31 +0500

    created server

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const http = require("http")
 
+const port  = 8000;
+
+const server = http.createServer();
+
+server.listen(port,(err)=>{
+    if(err){
+        console.log(err)
+        return;
+    }else{
+        console.log("server is runing on port: ",port)
+    }
+})
